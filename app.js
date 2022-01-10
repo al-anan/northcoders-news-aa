13471d1fad909980c217eb87b8bcf0916fe0488f
Alina Ananjeva <alina.ananjeva@eurodrivefinance.co.uk>
2022-01-10 17:17:33 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const apiRouter = require("./routes/api-router.js");
+
+app.use(cors());
 
 app.use(express.json());
 
